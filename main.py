cd8dbbf8c0c130503a0850e1fdc1adc103d3d3bc
Salemgnk <salem.gnandi@epitech.eu>
2024-05-11 13:28:30 +0100

    Creation of the interfacre

--- main.py
+++ main.py
@@ -22,20 +22,34 @@ def fade_out(image, screen):
         pygame.time.delay(10)
         alpha -= 5
 
+def pic_displayer(screen):
+    back = pygame.image.load("back.png").convert()
+    scaled_image = pygame.transform.scale(back, screen.get_size())
+    screen.blit(scaled_image, (0, 0))
+    pygame.display.update()
+
 def draw_rectangle(screen):
+    pic_displayer(screen)
     pygame.draw.rect(screen, (0, 0, 255), [100, 100, 400, 100], 2)
+    font = pygame.font.Font(None, 36)
+    text_surface = font.render("Password Manager", True, (255, 255, 255))
+    text_rect = text_surface.get_rect()
+    text_rect.center = (300, 150)
+    screen.blit(text_surface, text_rect)
+    pygame.display.update()
 
 def window():
     pygame.init()
     screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
     framerate = pygame.time.Clock()
     pygame.display.set_caption("Upsilon Solutions")
-    icon = pygame.image.load("upsilon.jpg").convert()
+    icon = pygame.image.load("upsilon.png").convert()
     pygame.display.set_icon(icon)
 
     fade_in(icon, screen)
     pygame.time.delay(2000)
-    fade_out(icon, screen)     
+    fade_out(icon, screen)
+    draw_rectangle(screen) 
     display = True
 
     while display:
