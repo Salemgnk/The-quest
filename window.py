4512f17e571fbaaea4a999a737ebb9a4855ae949
Salemgnk <salem.gnandi@epitech.eu>
2024-05-11 18:33:29 +0100

    Adjusting the first two buttons

--- window.py
+++ window.py
@@ -1,5 +1,18 @@
 import pygame
+import tkinter as tk
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+blue = (0, 0, 128)
+
+def displayer(screen):
+    font = pygame.font.Font(None, 36)
+    text = font.render('Welcome on Upsilon Tools. What do you want to do ?', True, white)
+    textRect = text.get_rect()
+    textRect.center = (screen.get_width() // 2, screen.get_height() - 950)
+    screen.blit(text, textRect)
+
+ 
 def fade_in(image, screen):
     alpha = 0
     while alpha < 255:
@@ -28,15 +41,20 @@ def pic_displayer(screen):
     screen.blit(scaled_image, (0, 0))
     pygame.display.update()
 
-def draw_rectangle(screen):
-    pic_displayer(screen)
-    pygame.draw.rect(screen, (0, 0, 255), [100, 100, 400, 100], 2)
+def draw_rectangle(screen, input, pos, text_pos):
+    pygame.draw.rect(screen, (0, 0, 255), pos, 2)
     font = pygame.font.Font(None, 36)
-    text_surface = font.render("Password Manager", True, (255, 255, 255))
+    text_surface = font.render(input, True, (255, 255, 255))
     text_rect = text_surface.get_rect()
-    text_rect.center = (300, 150)
+    text_rect.center = text_pos
     screen.blit(text_surface, text_rect)
     pygame.display.update()
+
+def buttons_choice(screen):
+    pic_displayer(screen)
+    draw_rectangle(screen, "Password Checker", [100, 250, 400, 100], (300, 300))
+    draw_rectangle(screen, "Password Generator", [100, 600, 400, 100], (300, 650))
+
 
 def window():
     pygame.init()
@@ -49,14 +67,16 @@ def window():
     fade_in(icon, screen)
     pygame.time.delay(2000)
     fade_out(icon, screen)
-    draw_rectangle(screen) 
+    buttons_choice(screen)
     display = True
 
     while display:
+        displayer(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 display = False
-            
+        displayer(screen)
+        pygame.display.flip()
         framerate.tick(30)
     pygame.quit()
 
