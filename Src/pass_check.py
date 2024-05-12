93ed4891eee04b93a7d538629917628ba92e1468
HOUNSA Darren <darren@kali>
2024-05-12 12:39:12 +0200

    Done with a New projet Architecture

---
+++ Src/pass_check.py
@@ -0,0 +1,21 @@
+import string
+import random
+import sys
+from displayer import *
+from test import *
+
+input_text = ""
+display = True
+
+def display_screen(screen):
+    screen.fill((0, 0, 0))
+    displayer(screen, "Enter your password:")
+
+    input_rect = pygame.Rect(200, 200, 400, 50)
+    font = pygame.font.Font(None, 36)
+
+    pygame.draw.rect(screen, white, input_rect, 2)        
+    text_surface = font.render(input_text, True, white)
+    screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+
+    pygame.display.flip()
